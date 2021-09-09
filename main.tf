f41b6718290240de05379b5de70bc45c57f02e3b
Leandro Mederios <leandro.devops@gmail.com>
2021-09-09 00:39:08 -0300

    test v3

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
-	#access_key  = "${var.access_key}"
-	#secret_key  = "${var.secret_key}"
-	#region      = "${var.region}"
+	access_key  = "${var.access_key}"
+	secret_key  = "${var.secret_key}"
+	region      = "${var.region}"
 }
 
 resource "aws_instance" "terra" {
